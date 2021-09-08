090ef38a57b469c23c2de52285c97a9ef6a975e9
kawarimidoll <kawarimidoll+git@gmail.com>
2021-09-08 09:48:59 +0900

    feat: add message when input is blank

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const input = document.getElementById("input");
 const output = document.getElementById("output");
 
 const callAPI = async () => {
+  if (!input.value) {
+    output.value = "Here will be twemoji URL!";
+    img.src = "";
+    return;
+  }
   const res = await fetch("./api?emoji=" + encodeURI(input.value));
   if (res.ok) {
     const src = await res.text();
     output.value = src;
     img.src = src;
   } else {
-    output.value = "There is no such emoji...";
+    output.value = "There is no such twemoji...";
     img.src = "";
   }
 };
