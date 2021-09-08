e86bd8636354d74b2f1bbdfbb8293626a2285c72
kawarimidoll <kawarimidoll+git@gmail.com>
2021-09-08 09:29:33 +0900

    feat: add main feature

---
+++ index.js
@@ -0,0 +1,21 @@
+const img = document.getElementById("img");
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+
+const callAPI = async () => {
+  const res = await fetch("./api?emoji=" + encodeURI(input.value));
+  if (res.ok) {
+    const src = await res.text();
+    output.value = src;
+    img.src = src;
+  } else {
+    output.value = "There is no such emoji...";
+    img.src = "";
+  }
+};
+
+document
+  .getElementById("input")
+  .addEventListener("input", window.throttle(callAPI, 2000));
+
+callApi();
--- server.ts
+++ server.ts
@@ -13,18 +13,45 @@ async function handleConn(conn: Deno.Conn) {
   }
 }
 
-async function handler(request: Request, conn: Deno.Conn) {
-  const { href, origin, host, pathname, hash, search } = new URL(request.url);
-  console.log({ href, origin, host, pathname, hash, search });
+function errResponse(status: number, statusText: string, init?: ResponseInit) {
+  init = { status, statusText, ...(init || {}) };
+  return new Response(`${status}: ${statusText}`, init);
+}
 
-  const readme = await Deno.readTextFile("./README.md");
+async function handler(request: Request, _conn: Deno.Conn) {
+  const { pathname, searchParams } = new URL(request.url);
+  // console.log({ href, origin, host, pathname, hash, search });
 
-  return new Response(readme, {
-    headers: {
-      "x-localaddr": `${conn.localAddr.hostname}:${conn.localAddr.port}`,
-      "x-remoteaddr": `${conn.remoteAddr.hostname}:${conn.remoteAddr.port}`,
-    },
-  });
+  if (pathname === "/") {
+    const html = await Deno.readTextFile("./index.html");
+    return new Response(html, { headers: { "content-type": "text/html" } });
+  }
+  if (pathname === "/api") {
+    const emoji = searchParams.get("emoji") || "";
+    let version = searchParams.get("version") || "";
+    if (!/^\d+\.\d+\.\d$/.test(version)) {
+      // use latest version
+      version = "13.1.0";
+    }
+    const p = [...emoji].map((x) => x.codePointAt(0)?.toString(16))[0];
+    console.log({ emoji, version, p });
+
+    const twemojiURL = `https://twemoji.maxcdn.com/v/${version}/72x72/${p}.png`;
+
+    const res = await fetch(twemojiURL);
+    if (res.ok) {
+      return new Response(twemojiURL);
+    }
+    return errResponse(400, "Invalid emoji parameter.");
+  }
+
+  if (pathname === "/index.js") {
+    const src = await Deno.readTextFile("./index.js");
+    return new Response(src, {
+      headers: { "content-type": "text/javascript" },
+    });
+  }
+  return errResponse(404, "Not found.");
 }
 
 for await (const conn of listener) {
