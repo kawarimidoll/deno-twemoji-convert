bd6d2849a7c8128e9954461180d41b4135e4c7ef
kawarimidoll <kawarimidoll+git@gmail.com>
2021-09-09 09:01:59 +0900

    test: add tests

--- deps.ts
+++ deps.ts
@@ -1,5 +1,4 @@
-// export {
-//   assert,
-//   assertEquals,
-//   assertThrows,
-// } from "https://deno.land/std@0.106.0/testing/asserts.ts";
+export {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.106.0/testing/asserts.ts";
--- test.ts
+++ test.ts
@@ -1,1 +1,75 @@
-console.log("there is no test now");
+import { assert, assertEquals } from "./deps.ts";
+import {
+  errResponse,
+  handle404,
+  handleApi,
+  handleJs,
+  handleRoot,
+} from "./handlers.ts";
+
+// disable log
+console.log = (..._args: unknown[]) => {};
+
+Deno.test("[errResponse] 404: Not found", () => {
+  assertEquals(
+    errResponse(404, "Not found"),
+    ["404: Not found", { status: 404, statusText: "Not found" }],
+  );
+});
+
+Deno.test("[handle404] 404: Not found", () => {
+  assertEquals(
+    handle404(),
+    ["404: Not found", { status: 404, statusText: "Not found" }],
+  );
+});
+
+Deno.test("[handleRoot] successful", async () => {
+  // no test about contents
+  const [html, init] = await handleRoot();
+  assert(html);
+  assertEquals(
+    init,
+    { headers: { "content-type": "text/html" } },
+  );
+});
+
+Deno.test("[handleJs] successful", async () => {
+  // no test about contents
+  const [js, init] = await handleJs();
+  assert(js);
+  assertEquals(
+    init,
+    { headers: { "content-type": "text/javascript" } },
+  );
+});
+
+Deno.test("[handleApi] Invalid emoji parameter", async () => {
+  const params = new URLSearchParams();
+  params.set("emoji", "");
+  assertEquals(
+    await handleApi(params),
+    [
+      "400: Invalid emoji parameter",
+      { status: 400, statusText: "Invalid emoji parameter" },
+    ],
+  );
+  params.set("emoji", "0");
+  assertEquals(
+    await handleApi(params),
+    [
+      "400: Invalid emoji parameter",
+      { status: 400, statusText: "Invalid emoji parameter" },
+    ],
+  );
+});
+
+Deno.test("[handleApi] successful", async () => {
+  // no test about contents
+  const params = new URLSearchParams();
+  params.set("emoji", "🦕");
+  assertEquals(
+    await handleApi(params),
+    ["https://twemoji.maxcdn.com/v/13.1.0/72x72/1f995.png", {}],
+  );
+});
